e569d769a5006c30b09da06076b28cf8a12b895e
vladnomad <vlad.nmd@gmail.com>
2023-10-02 11:23:25 +0200

    Additional price information / Info mobile styles

--- src/components/about.js
+++ src/components/about.js
@@ -58,7 +58,7 @@ class About extends Component {
                 <div className="about-wrapper">
                     <article className="about-article" aria-labelledby="about-title">
                         <h2 id="about-title" className="about-title">About the App</h2>
-                        <p><b>Welcome to our PS2 Horror Collection Manager!</b><br/>This app empowers you to curate and explore your PS2 horror collection, discover new titles, and plan your expenses and collector's journey ahead. Experience the captivating world of survival horror on the PS2, where storytelling, audiovisual design, and emotional engagement converge to create unforgettable experiences.</p>
+                        <p><b>Welcome to our PS2 Collection App!</b><br/>This app empowers you to curate and explore your PS2 horror collection, discover new titles, and plan your collector's journey. Experience the captivating world of survival horror on the PS2, where storytelling, audiovisual design, and emotional engagement converge to create unforgettable experiences. As you embark on your collector's adventure, keep in mind that the love of collecting goes beyond the monetary value of each game. It's about cherishing the memories, experiences, and stories that these games hold.</p>
                     </article>
                     <article aria-labelledby="controls-title">
                         <h2 id="controls-title" className="about-title">Controls &amp; Tips</h2>
@@ -85,16 +85,31 @@ class About extends Component {
                             <h3 className="mt-2">Price buttons:</h3>
                             <div className="about-control-option">
                                 <img className="about-control-icon price" src={looseIcon} alt=""/>
-                                <p>Loose: Choose this option if you own a disc itself, without or with damaged casing.</p>
+                                <p>Loose: A disc itself (may not include the game's original casing or manual).</p>
                             </div>
                             <div className="about-control-option">
                                 <img className="about-control-icon price" src={cibIcon} alt=""/>
-                                <p>CIB: Choose this option if you own a complete in box used copy of the game.</p>
+                                <p>CIB: Complete In Box used copy of the game with original casing and manual.</p>
                             </div>
                             <div className="about-control-option">
                                 <img className="about-control-icon price" src={newgIcon} alt=""/>
-                                <p>New: Choose this option if you own a sealed copy of the game / never used copy in perfect condition.</p>
+                                <p>New: Sealed and graded copies by WATA in perfect condition (if none available, a regular sealed copy with a hologram sticker).</p>
                             </div>
+                        </div>
+                    </article>
+                    <article className="prices-article" aria-labelledby="prices-title">
+                        <h2 id="prices-title" className="about-title">Game Prices</h2>
+                        <div className="about-prices">
+                            <h3 className="prices-subtitle">Market Analysis</h3>
+                            <p>To establish collection value categories, we conducted extensive research in October 2023 by analyzing prices on the most popular gaming marketplace, eBay. We specifically utilized the "Buy It Now" filter on eBay to exclude auction listings and ensure that the chosen prices reflected readily available options.</p>
+                        </div>
+                        <div className="about-prices">
+                            <h3 className="prices-subtitle">Price Criteria</h3>
+                            <p>Our analysis revealed that the NTSC U/C region had the most comprehensive coverage in terms of offers, demand, and pricing. For games with limited NTSC U/C availability, we occasionally used alternative region versions to fill in missing prices. It's important to acknowledge that NTSC J and PAL regions have their own pricing dynamics, which can differ significantly from NTSC U/C.</p>
+                        </div>
+                        <div className="about-prices">
+                            <h3 className="prices-subtitle">Representational Nature of Prices</h3>
+                            <p>The provided prices are for reference purposes and may not reflect real-time market values. The gaming market is dynamic, and prices can fluctuate due to factors such as demand, rarity, and game condition. Please consider these values as general estimates rather than definitive market prices when managing your collection.</p>
                         </div>
                     </article>
                     <footer>
