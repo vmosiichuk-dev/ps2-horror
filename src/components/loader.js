e5d09b1c365fdec2b6842ac4fe8ca505dc836967
vladnomad <vlad.nmd@gmail.com>
2023-12-04 14:16:29 +0100

    Setting timeout for loading text change using hooks

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,19 +1,28 @@
+import React, {useState, useEffect} from "react"
 import loaderGif from "../assets/img/loader.gif"
 import "../assets/styles/loader.css"
 
 function Loader({ welcomeClick, loaderError }) {
+    const [loaderMessage, setLoaderMessage] = useState("Fetching horror titles from the digital archive...")
     let loaderClass = "loader",
-        loaderMessageClass = "loader__error",
-        loaderMessage = ""
+        loaderMessageClass = "loader__error"
 
     if (welcomeClick) {
         loaderClass += " is-active"
         loaderMessageClass += " is-active"
-        loaderMessage = "Fetching data from the PS2 database..."
     }
-    if (loaderError) {
-        loaderMessage = "An error occured while connecting to the database. Try reloading the page or visit our PS2 Collection App later."
-    }
+
+    useEffect(() => {
+        if (welcomeClick) {
+            const secondMessageTimeout = setTimeout(() => {
+                setLoaderMessage("Arranging the fear factors for your lineup...")
+            }, 1900)
+
+            return () => clearTimeout(secondMessageTimeout)
+        } else if (loaderError) {
+            setLoaderMessage("An error occured while connecting to the database. Try reloading the page or visit our PS2 Collection App later.")
+        }
+    }, [welcomeClick, loaderError])
 
     return (
         <div className={loaderClass} role="status">
