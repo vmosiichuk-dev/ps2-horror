7d2fc6131e0db739461076386fa11d1dfe3d3d1c
vladnomad <vlad.nmd@gmail.com>
2023-12-11 17:30:09 +0100

    Refactoring JS: RootContent rewritten using Hooks

--- src/components/root-content.js
+++ src/components/root-content.js
@@ -1,66 +1,81 @@
-import { Component } from "react"
+import React, { useState, useEffect } from "react"
 import App from "./app"
 import Welcome from "./welcome"
 
-class RootContent extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-			animationReset: false,
-			apiLoaded: false,
-			transitionStart: false,
-            welcomeClick: false,
-			loaderError: false,
-			buttonText: ""
-		}
-	}
+function RootContent() {
+	const [state, setState] = useState({
+		animationReset: false,
+		apiLoaded: false,
+		transitionStart: false,
+		welcomeClick: false,
+		loaderError: false,
+		appAria: true,
+		buttonText: ""
+	})
 
-	handleStateChange = (state) => {
+	const handleStateChange = (state) => {
 		if (state === "welcomeClick") {
 			window.sessionStorage.setItem("PS2_SURVIVAL_HORROR_WELCOME_SCREEN", JSON.stringify(true))
-			return this.setState({ [state]: true, animationReset: true })
-		}
 
-		this.setState({ [state]: true })
+			setState(prevState => ({
+				...prevState,
+				[state]: true,
+				animationReset: true
+			}))
+		} else {
+			setState(prevState => ({ ...prevState, [state]: true }))
+		}
 	}
 
-	handleError = () => this.setState({ loaderError: true })
+	const handleError = () => setState(prevState => ({...prevState, loaderError: true}))
 
-	componentDidMount() {
+	useEffect(() => {
+		if (!welcomeClick || !apiLoaded) setState(prevState => ({...prevState, appAria: false}))
+
 		const welcomeScreen = JSON.parse(window.sessionStorage.getItem("PS2_SURVIVAL_HORROR_WELCOME_SCREEN"))
 
 		if (welcomeScreen) {
-			this.setState({ welcomeClick: true, animationReset: true })
+			setState(prevState => ({
+				...prevState,
+				welcomeClick: true,
+				animationReset: true
+			}))
 		}
 		
-		const startButtonText = "START",
-			  continueButtonText = "CONTINUE",
-			  localGameData = JSON.parse(window.localStorage.getItem("PS2_SURVIVAL_HORROR_GAME_DATA"))
+		const startButtonText = "START"
+		const continueButtonText = "CONTINUE"
+		const localGameData = JSON.parse(window.localStorage.getItem("PS2_SURVIVAL_HORROR_GAME_DATA"))
 			  
 		if (localGameData !== null && localGameData.length > 0) {
-			this.setState({ buttonText: continueButtonText })
+			setState(prevState => ({...prevState, buttonText: continueButtonText}))
 		} else {
-			this.setState({ buttonText: startButtonText })
+			setState(prevState => ({...prevState, buttonText: startButtonText}))
 		}
-	}
+	}, [])
+	
+	const { animationReset, apiLoaded, transitionStart, welcomeClick, loaderError, buttonText, appAria } = state
 
-	render() {
-		const { animationReset, apiLoaded, transitionStart, welcomeClick, loaderError, buttonText, subtitleText } = this.state
-		let welcome = null, appAria = true
-		
-		if (!welcomeClick || !apiLoaded) {
-			welcome = <Welcome animationReset={animationReset} transitionStart={transitionStart} welcomeClick={welcomeClick} loaderError={loaderError} buttonText={buttonText} subtitleText={subtitleText} onStateChange={this.handleStateChange} />
-		} else {
-			appAria = false
-		}
-
-		return (
-			<>
-				{welcome}
-				<App transitionStart={transitionStart} welcomeClick={welcomeClick} onStateChange={this.handleStateChange} onError={this.handleError} appAria={appAria} />
-			</>
-		)
-	}
+	return (
+		<>
+			{ !welcomeClick || !apiLoaded 
+				? <Welcome 
+					animationReset={animationReset} 
+					transitionStart={transitionStart} 
+					welcomeClick={welcomeClick} 
+					loaderError={loaderError} 
+					buttonText={buttonText} 
+					onStateChange={handleStateChange} 
+				/> 
+			: null }
+			<App 
+				transitionStart={transitionStart} 
+				welcomeClick={welcomeClick} 
+				onStateChange={handleStateChange} 
+				onError={handleError} 
+				appAria={appAria} 
+			/>
+		</>
+	)
 }
 
 export default RootContent
